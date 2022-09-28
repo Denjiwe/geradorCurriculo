325bbfe42c1e630d8c11e3c5534d964372aa6687
Santhiago Monteiro <santhiago.per@edu.unipar.br>
2022-09-27 23:59:51 -0300

    ADD validação campo telefone

--- index.php
+++ index.php
@@ -9,8 +9,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-    
-        
+    <script src="js/validaCampos.js"></script>  
 
     
 </head>
@@ -24,7 +23,7 @@
     </nav>
 
     <main class="mt-5 pt-4 container">
-        <form method="post" action="" id="formCurriculo">
+        <form method="post" action="geraCurriculo.php" id="formCurriculo">
             <div class="mb-5 col-7">
                 <label for="nome" class="form-label">Nome Completo</label>
                 <input type="text" class="form-control" name="nome" id="nome" placeholder="Digite seu nome" required>
@@ -54,8 +53,8 @@
                 </div>
 
                 <div class="mb-5 col-3">
-                    <label for="telefone" class="form-label">Telefone</label>
-                    <input type="tel" class="form-control" name="telefone" placeholder="(12)34567-8900" id="telefone" required>
+                    <label for="telefone" class="form-label">Celular</label>
+                    <input type="tel" class="form-control" name="telefone" placeholder="(12)34567-8900" id="telefone" maxlength="15" onkeypress="mascara(this)" required>
                 </div>
             </div>
 
