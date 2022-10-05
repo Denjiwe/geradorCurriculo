6917a37193c2062cbe41961a2a49f9b516e8c015
Santhiago Monteiro <santhiago.per@edu.unipar.br>
2022-10-05 00:27:13 -0300

    ADD print button

--- geraCurriculo.php
+++ geraCurriculo.php
@@ -31,14 +31,6 @@ if (isset($_POST['empresa'])){
     $totalProfissoes = count($empresa);
 }
 
-/*for($i = 1; $i <= $totalInstituicao; $i++)
-{
-    printf("Título : %s <br />", $instituicao[$i]);
-    printf("Descrição : %s <br />", $categoria[$i]);
-    echo '<br /><hr />';
-}*/
-
-
 ?>
 
 <!DOCTYPE html>
@@ -47,21 +39,13 @@ if (isset($_POST['empresa'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
     <title>Currículo</title>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/stylesCurriculo.css">
-
-    <script type="text/javascript">
-        $.ajax({
-            url: 'geraCurriculo.php'
-            success: function (data) {
-                $('#nome').html(data);
-            }
-        })
-    </script>
 
 </head>
 <body>
@@ -97,19 +81,19 @@ if (isset($_POST['empresa'])){
         <div id="profissoes">
             <h2 id="profissoesTitle">Experiências Profissionais</h2>
             <?php
-                    for($x = 1; $x <= $totalProfissoes; $x++)
-                    {
-                        echo '<span class="spanProfissoes">';
-                        printf(" • %s -", $empresa[$x]);
-                        printf(" %s ", $cargo[$x]);
-                        printf(" ( %s ", $deProfissoes[$x]);
-                        printf(" - %s )", $ateProfissoes[$x]);
-                        echo '</span><br>';
+                for($x = 1; $x <= $totalProfissoes; $x++)
+                {
+                    echo '<span class="spanProfissoes">';
+                    printf(" • %s -", $empresa[$x]);
+                    printf(" %s ", $cargo[$x]);
+                    printf(" ( %s ", $deProfissoes[$x]);
+                    printf(" - %s )", $ateProfissoes[$x]);
+                    echo '</span><br>';
 
-                    }
-                ?>
+                }
+            ?>
         </div>
-
+        <button type="button" class="btn btn-primary nPrintable" onclick=window.print()>Baixar</button>
     </div>
     
 </body>
